1bea5fd81fd391b155eb319e4c99e614b8d74a8e
viktor hugo <victorHbenitez85@hotmail.com>
2017-05-19 19:43:33 -0500

    Completed UI custom of Sign Up

---
+++ instagramApp/SingUpVC.swift
@@ -0,0 +1,38 @@
+//
+//  SingUpVC.swift
+//  instagramApp
+//
+//  Created by Victor Hugo Benitez Bosques on 19/05/17.
+//  Copyright © 2017 Victor Hugo Benitez Bosques. All rights reserved.
+//
+
+import UIKit
+
+class SingUpVC: UIViewController {
+
+
+    @IBOutlet weak var fullNameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passField: UITextField!
+    @IBOutlet weak var confirmPassField: UITextField!
+    @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    // MARK: - IBActions
+    @IBAction func selectedimagePressed(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func nextBtnPressed(_ sender: UIButton) {
+    }
+    
+
+}
