cbb1846ac73d9eb4687fb83d7ce4c3ed7a9e3691
viktor hugo <victorHbenitez85@hotmail.com>
2017-06-14 23:17:35 -0500

    Display all post in Feed Section VC

---
+++ instagramApp/Post.swift
@@ -0,0 +1,21 @@
+//
+//  Post.swift
+//  instagramApp
+//
+//  Created by Victor Hugo Benitez Bosques on 14/06/17.
+//  Copyright © 2017 Victor Hugo Benitez Bosques. All rights reserved.
+//
+
+import UIKit
+
+// Create this class because, we're going to create an array from the post branch from Firebase and We'll going to display them in the Feed Section
+class Post: NSObject {
+    
+    var author : String!
+    var likes : String!
+    var pathImage : String!
+    var userID : String!
+    var postID : String!
+    
+    
+}
---
+++ instagramApp/PostCVCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCVCell.swift
+//  instagramApp
+//
+//  Created by Victor Hugo Benitez Bosques on 14/06/17.
+//  Copyright © 2017 Victor Hugo Benitez Bosques. All rights reserved.
+//
+
+import UIKit
+
+class PostCVCell: UICollectionViewCell {
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var authorLbl: UILabel!
+    @IBOutlet weak var likesLbl: UILabel!
+    @IBOutlet weak var likesBtn: UIButton!
+    @IBOutlet weak var unlikesBtn: UIButton!
+    
+    
+    
+    @IBAction func likesBtnPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func unlikesBtnPressed(_ sender: Any) {
+        
+        
+    }
+    
+}
